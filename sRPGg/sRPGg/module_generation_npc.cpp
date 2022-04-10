8d8fae33cc924c31e3b9808137ccb647e0b2b868
Anton <githubantonov@mail.ru>
2022-04-10 19:28:16 +0300

    FIX CONFLICT

---
+++ sRPGg/sRPGg/module_generation_npc.cpp
@@ -0,0 +1,27 @@
+#include "module_generation_npc.h"
+
+void generation_npc::NPC_generation()
+{
+	/*Код генерации неигровых персонажей*/
+}
+
+void generation_npc::load_model_NPC()
+{
+	/*Код загрузки и прорисовки моделей неигровых ерсонажей*/
+}
+
+void generation_npc::load_item_NPC()
+{
+	/*Код раздачи и прогурзки моделей предметов выдаваемые
+	неигровым персонажам*/
+}
+
+void generation_npc::load_location_spawn_NPC()
+{
+	/*Код загрузки и ростановки спавна неигровых персонажей*/
+}
+
+void generation_npc::spawn_NPC()
+{
+	/*Код спавна неигровых персонажей в игровом мире*/
+}
