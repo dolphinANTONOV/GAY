2b71f9d978faa7732700e8cba9f215261b2e37ac
Anton <githubantonov@mail.ru>
2022-04-10 15:47:23 +0300

    Написание функции прорисовки и загрузки моделей неигровых персонажей

--- sRPGg/sRPGg/module_generation_npc.cpp
+++ sRPGg/sRPGg/module_generation_npc.cpp
@@ -4,3 +4,8 @@ void generation_npc::NPC_generation()
 {
 	/*Код генерации неигровых персонажей*/
 }
+
+void generation_npc::load_model_NPC()
+{
+	/*Код загрузки и прорисовки моделей неигровых ерсонажей*/
+}
